ca57205e905d78775dac72e618f6d2d06cc52eae
Hossam <Hossam@DESKTOP-17NKLN7>
2024-02-24 14:55:38 +0200

    #Variables&DataTypes

--- C#Fundamentals/Program.cs
+++ C#Fundamentals/Program.cs
@@ -1,2 +1,37 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+#region Variables
+// Variables : is a Data Holder 
+
+
+#region Variable Decleration 
+
+/*Variable Decleration 
+    DataType VariableName = VariableValue ; 
+
+    DataType VariableName  ===> Variable Declaration 
+    VariableValue ;  ===> Vaiable Assignment
+    
+    ex ==>  string Name = "Hossam Emara" ;
+
+*/
+
+string Name = "Hossam Emara";
+Console.WriteLine(Name);
+
+#endregion
+
+#region Data Types 
+
+
+
+#endregion
+
+
+
+
+
+
+
+
+
+#endregion

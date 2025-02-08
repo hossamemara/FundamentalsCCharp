9e251bb8b61c88d7b330248e4330e7bad2fed160
hossam.emara <hossam.emara@DESKTOP-DFO8D39>
2025-02-08 16:19:51 +0200

    #

--- C#Fundamentals/Program.cs
+++ C#Fundamentals/Program.cs
@@ -1,4 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿//See https://aka.ms/new-console-template for more information
 using st = C_Fundamentals.Classes.InternalClasses.Student; // Aliass Names
 
 #region 1.Difference Between Compiler & Interpreter 

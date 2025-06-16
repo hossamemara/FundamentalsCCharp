ca2901cd1f254371d51442a73de72f81a0b86613
hossam.emara <hossam.emara@DESKTOP-DFO8D39>
2025-06-16 18:22:34 +0300

    #

--- C#Fundamentals/Classes/Employee.cs
+++ C#Fundamentals/Classes/Employee.cs
@@ -8,7 +8,7 @@ namespace C_Fundamentals.Classes
 {
     internal abstract class Employee : Person
     {
-        public  string Email;
+        public  string Email { get; set; }
         public abstract decimal GetSalary();
         public abstract IEnumerable<PayItem> GetPayItems();
         public abstract string GetName();
